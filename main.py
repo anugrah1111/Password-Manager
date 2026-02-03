16d9a8c3d68a09d563e96eced3c4a32df6226bc6
anugrah1111 <anugrahdayal@gmail.com>
2026-02-03 11:05:44 +0530

    Use main.py as GUI entry point

--- main.py
+++ main.py
@@ -1,62 +1,19 @@
 """
 main.py
 -------
-Entry point of the Password Manager application.
-Handles user interaction through command-line interface.
+Application entry point.
+Launches the Tkinter GUI.
 """
 
-from manager import PasswordManager
-from auth import AuthService
-
+from gui import PasswordManagerGUI
+import tkinter as tk
 
 
 def main():
-    """
-    Main function:
-    - Authenticates user with master password
-    - Starts password manager on success
-    """
-    auth = AuthService()
-
-    # First-time setup
-    if not auth.is_master_password_set():
-        auth.setup_master_password()
-
-    # Authentication check
-    if not auth.authenticate():
-        return  # Exit app if authentication fails
-    manager = PasswordManager()
-
-    while True:
-        print("""
-==========================
-🔐 PASSWORD MANAGER
-==========================
-1. Add Password
-2. View Password
-3. Exit
-""")
-
-        choice = input("Choose an option: ").strip()
-
-        if choice == "1":
-            website = input("Website: ").strip()
-            username = input("Username: ").strip()
-            password = input("Password: ").strip()
-            manager.add_password(website, username, password)
-
-        elif choice == "2":
-            website = input("Enter website name: ").strip()
-            manager.view_password(website)
-
-        elif choice == "3":
-            print("👋 Exiting Password Manager")
-            break
-
-        else:
-            print("❌ Invalid choice, try again")
+    root = tk.Tk()
+    app = PasswordManagerGUI(root)
+    root.mainloop()
 
 
-# Ensures main() runs only when this file is executed directly
 if __name__ == "__main__":
     main()
